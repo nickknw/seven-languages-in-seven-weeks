99c660a5797b2a9e2d4aaa4de3681d286876a746
Nick <nickknw@gmail.com>
2011-04-03 20:42:18 -0700

    add prolog day3 specs

---
+++ week-3-prolog/day3.pl
@@ -0,0 +1,18 @@
+# Find:
+#
+# 1. Prolog has some input/output features as well. Find print predicates that
+# print out variables.
+#
+# 2. Find a way to use the print predicates to print only successful solutions.
+# How do they work? 
+
+# Do:
+#
+# 1. Modify the Sudoku solver to work on six-by-six puzzles (squares are 3x2)
+# and 9x9 puzzles)
+#
+# 2. Make the Sudoku solver print prettier solutions.
+#
+# 3. Solve the Eight Queens problem by taking a list of queens. Rather than a
+# tuple, represent each queen with an integer, from 1-8. Get the row of a queen
+# by its position in the list and the column by the value in the list.
