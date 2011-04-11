1be4a8b19a71131bab15e5de22a7675da3ef578c
Nick <nickknw@gmail.com>
2011-04-10 22:34:31 -0700

    partway through day 3 of week 3
    just putting in the mini sudoku solver from the book and trying
    to get it to work. Ran into problems

--- week-3-prolog/day3.pl
+++ week-3-prolog/day3.pl
@@ -43,7 +43,52 @@
 %
 % 1. Modify the Sudoku solver to work on six-by-six puzzles (squares are 3x2)
 % and 9x9 puzzles)
-%
+
+    % SWI Prolog doesn't have fd_domain or fd_all_different. Let's make them!
+    fd_domain([], _, _) :- true.
+    fd_domain([X|XS], Low, High) :- between(Low, High, X), fd_domain(XS, Low, High).
+
+    fd_all_different([]) :- true.
+    fd_all_different([X|XS]) :- not(member(X, XS)), fd_all_different(XS).
+
+    % after testing that, they don't work well because they are very slow. The
+    % constraint programming library does something special to make this work
+    % fast, so let's use it.
+    use_module(library(clpfd)).
+
+    valid([]).
+    valid([Head|Tail]) :-
+        all_different(Head),    % in the book, this is 'fd_all_different'
+        valid(Tail).
+
+    % beginning of sudoku rule itself
+    sudoku(Puzzle, Solution) :- 
+    Solution = Puzzle,
+    Puzzle = [S11, S12, S13, S14,
+              S21, S22, S23, S24,
+              S31, S32, S33, S34,
+              S41, S42, S43, S44],
+    Puzzle ins 1..4,    % in the book, this is 'fd_domain'
+
+    Row1 = [S11, S12, S13, S14],
+    Row2 = [S21, S22, S23, S24],
+    Row3 = [S31, S32, S33, S34],
+    Row4 = [S41, S42, S43, S44],
+
+    Col1 = [S11, S21, S31, S41],
+    Col2 = [S12, S22, S32, S42],
+    Col3 = [S13, S23, S33, S43],
+    Col4 = [S14, S24, S34, S44],
+
+    Square1 = [S11, S12, S21, S22],
+    Square2 = [S13, S14, S23, S24],
+    Square3 = [S31, S32, S41, S42],
+    Square4 = [S33, S34, S43, S44],
+
+    valid([Row1, Row2, Row3, Row4,
+           Col1, Col2, Col3, Col4,
+           Square1, Square2, Square3, Square4]).
+
 % 2. Make the Sudoku solver print prettier solutions.
 %
 % 3. Solve the Eight Queens problem by taking a list of queens. Rather than a
