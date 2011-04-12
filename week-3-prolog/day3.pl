ad8af2ee634f65b89d7a3426913778ab88b3b761
Nick <nickknw@gmail.com>
2011-04-12 00:02:15 -0700

    SUDOKU SOLVER IS WORKING OMFG

--- week-3-prolog/day3.pl
+++ week-3-prolog/day3.pl
@@ -58,7 +58,8 @@
               S71, S72, S73, S74, S75, S76, S77, S78, S79,
               S81, S82, S83, S84, S85, S86, S87, S88, S89,
               S91, S92, S93, S94, S95, S96, S97, S98, S99],
-    Puzzle ins 1..4,
+
+    Puzzle ins 1..9,
 
     Row1 = [S11, S12, S13, S14, S15, S16, S17, S18, S19],
     Row2 = [S21, S22, S23, S24, S25, S26, S27, S28, S29],
@@ -96,6 +97,7 @@
            Col1, Col2, Col3, Col4, Col5, Col6, Col7, Col8, Col9,
            Square1, Square2, Square3, Square4, Square5, Square6, Square7, Square8, Square9]),
 
+    label(Puzzle),
     prettier_print(Puzzle).
 
     valid([]).
@@ -106,7 +108,7 @@
 % 2. Make the Sudoku solver print prettier solutions.
 
     prettier_print([]).
-    prettier_print([Puzzle]) :- prettier_print(0, [Puzzle]).
+    prettier_print(Puzzle) :- prettier_print(0, Puzzle).
 
     prettier_print(0, Puzzle) :- 
         writeln('┌───────┬───────┬───────┐'), 
@@ -116,12 +118,13 @@
         prettier_print(5, Puzzle).
     prettier_print(8, Puzzle) :- 
         writeln('│───────┼───────┼───────│'), 
-        prettier_print(10, Puzzle).
-    prettier_print(13, _) :- 
+        prettier_print(9, Puzzle).
+    prettier_print(12, []) :- 
         writeln('└───────┴───────┴───────┘').
 
     prettier_print(N, [Col1, Col2, Col3, Col4, Col5, Col6, Col7, Col8, Col9 | Puzzle]) :- 
-        N =\= 0, N =\= 4, N =\= 9, N =\= 13,
+        member(N, [1,2,3,5,6,7,9,10,11]),
+        %N =\= 0, N =\= 4, N =\= 8, N =\= 13,
         % note to self about above: remember, prolog's pattern matching isn't
         % like pattern matching in other languages
 
@@ -129,9 +132,48 @@
         succ(N, N1),
         prettier_print(N1, Puzzle).
 
-% test print array:
-% [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9]
-    
+% test array:
+% [7, 3, _, _, 1, _, _, _, _, 8, _, _, 2, _, _, 4, _, _, 4, _, _, _, _, 5, _, _, _, _, _, 4, _, _, 7, 3, 8, _, _, 8, _, _, 3, _, _, 4, _, _, 1, 2, _, _, _, 7, _, _, _, _, _, 8, _, _, _, _, 9, _, _, 9, _, _, 2, _, _, 4, _, _, _, _, 5, _, _, 2, 3] 
+
+
 % 3. Solve the Eight Queens problem by taking a list of queens. Rather than a
 % tuple, represent each queen with an integer, from 1-8. Get the row of a queen
 % by its position in the list and the column by the value in the list.
+
+    valid_queen(Col) :- member(Col, [1,2,3,4,5,6,7,8]).
+
+    valid_board([]).
+    valid_board([Head|Tail]) :- valid_queen(Head), valid_board(Tail).
+
+    cols([], []).
+    cols([(_, Col)|QueensTail], [Col|ColsTail]) :-
+        cols(QueensTail, ColsTail).
+
+    diags1([], []).
+    diags1([(Row, Col)|QueensTail], [Diagonal|DiagonalsTail]) :-
+        Diagonal is Col - Row,
+        diags1(QueensTail, DiagonalsTail).
+
+    diags2([], []).
+    diags2([(Row, Col)|QueensTail], [Diagonal|DiagonalsTail]) :-
+        Diagonal is Col + Row,
+        diags2(QueensTail, DiagonalsTail).
+
+    eight_queens(Board) :-
+        Board = [(1, _), (2, _), (3, _), (4, _), (5, _), (6, _), (7, _), (8, _)],
+        valid_board(Board),
+
+        cols(Board, Cols),
+        diags1(Board, Diags1),
+        diags2(Board, Diags2),
+
+        fd_all_different(Cols),
+        fd_all_different(Diags1),
+        fd_all_different(Diags2).
+
+% Alright, I'm not even sure how I'm supposed to call this now.
+% I thought it should work with just 'Board' or a variable. Then I tried the
+% last invocation. All I get is 'false'. I have no idea where it is going wrong.
+% I think I am going to have to leave Prolog for now and come back to it later
+%
+% [(1, A), (2, B), (3, C), (4, D), (5, E), (6, F), (7, G), (8, H)]
