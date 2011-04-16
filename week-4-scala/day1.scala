ab2b9c7c1f576f901aaad6470584ed1d050f67af
Nick <nickknw@gmail.com>
2011-04-16 10:49:54 -0700

    change comment style

--- week-4-scala/day1.scala
+++ week-4-scala/day1.scala
@@ -1,35 +1,35 @@
-/* Find:
- *
- * 1. The Scala API
- *
- *      http://www.scala-lang.org/api/current/index.html
- *
- * 2. A comparison of Java and Scala
- *
- *    Quite a nice article, well-written, good detail level
- *      http://www.ibm.com/developerworks/java/library/j-scala01228/index.html
- *
- *    Overiew/summary level comparison of Java, Scala, Groovy, Clojure
- *      http://stackoverflow.com/questions/1314732/scala-vs-groovy-vs-clojure
- *
- *    This is funny and kind of revealing, even if the question leads to a bit of a biased picture.
- *      http://stackoverflow.com/questions/2952732/samples-of-scala-and-java-code-where-scala-code-looks-simpler-has-fewer-lines
- *
- *    More low-level comparison
- *      http://blogs.sun.com/sundararajan/entry/scala_for_java_programmers
- *
- *  
- *      http://www.ibm.com/developerworks/java/library/j-scala01228/index.html
- *
- * 3. A discussion of val versus var
- * 
- *      http://stackoverflow.com/questions/1791408/what-is-the-difference-between-a-var-and-val-definition-in-scala
- */
+// Find:
+//
+// 1. The Scala API
+//
+//      http://www.scala-lang.org/api/current/index.html
+//
+// 2. A comparison of Java and Scala
+//
+//    Quite a nice article, well-written, good detail level
+//      http://www.ibm.com/developerworks/java/library/j-scala01228/index.html
+//
+//    Overiew/summary level comparison of Java, Scala, Groovy, Clojure
+//      http://stackoverflow.com/questions/1314732/scala-vs-groovy-vs-clojure
+//
+//    This is funny and kind of revealing, even if the question leads to a bit of a biased picture.
+//      http://stackoverflow.com/questions/2952732/samples-of-scala-and-java-code-where-scala-code-looks-simpler-has-fewer-lines
+//
+//    More low-level comparison
+//      http://blogs.sun.com/sundararajan/entry/scala_for_java_programmers
+//
+//  
+//      http://www.ibm.com/developerworks/java/library/j-scala01228/index.html
+//
+// 3. A discussion of val versus var
+// 
+//      http://stackoverflow.com/questions/1791408/what-is-the-difference-between-a-var-and-val-definition-in-scala
+//
 
-/* Do:
- *
- * 1. Write a game that will take a tic-tac-toe board with X, O, and blank
- *    characters and detect the winner or whtether there is a tie or no winner yet.
- *    Use classes where appropriate.
- * 
- * 2. Bonus Problem: Let two players play tic-tac-toe.
+// Do:
+//
+// 1. Write a game that will take a tic-tac-toe board with X, O, and blank
+//    characters and detect the winner or whtether there is a tie or no winner yet.
+//    Use classes where appropriate.
+// 
+// 2. Bonus Problem: Let two players play tic-tac-toe.
