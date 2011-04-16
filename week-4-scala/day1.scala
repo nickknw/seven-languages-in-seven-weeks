bc15091cf0d407f38a576b1535c187d1709cc6b1
Nick <nickknw@gmail.com>
2011-04-16 13:14:01 -0700

    done question 1 of day 1

--- week-4-scala/day1.scala
+++ week-4-scala/day1.scala
@@ -29,7 +29,72 @@
 // Do:
 //
 // 1. Write a game that will take a tic-tac-toe board with X, O, and blank
-//    characters and detect the winner or whtether there is a tie or no winner yet.
+//    characters and detect the winner or whether there is a tie or no winner yet.
 //    Use classes where appropriate.
-// 
+//
+//  run with: 'scala -howtorun:script day1.scala'
+
+object TicTac {
+    def whoWon(board: Array[String]) :String = {
+        
+        for(i <- 0 to 2) {
+            if(same(board(i)(0), board(i)(1), board(i)(2))) {
+                return board(i)(0) + "s won!"
+            }
+        }
+
+        for(i <- 0 to 2) {
+            if(same(board(0)(i), board(1)(i), board(2)(i))) {
+                return board(0)(i) + "s won!"
+            }
+        }
+
+        // diagonals
+        if(same(board(0)(0), board(1)(1), board(2)(2))) {
+            return board(0)(0) + "s won!"
+        }
+
+        if(same(board(2)(0), board(1)(1), board(0)(2))) {
+            return board(2)(0) + "s won!"
+        }
+
+        if(board.map(row => row.contains(' ')).contains(true)) {
+            return "No winner yet!"
+        }
+
+        return "It's a tie!"
+    }
+
+    def same(a:Char, b:Char, c:Char) :Boolean = {
+        return a == b && b == c
+    }
+}
+
+println(TicTac.whoWon(Array(
+    "XOX",
+    "XOO",
+    "XXO")))
+
+println(TicTac.whoWon( Array(
+    "XOX",
+    "XOO",
+    " XO")))
+
+println(TicTac.whoWon( Array(
+    "XOX",
+    "XOO",
+    "OXO")))
+
+println(TicTac.whoWon( Array(
+    "XXO",
+    "XOO",
+    "OXX")))
+
 // 2. Bonus Problem: Let two players play tic-tac-toe.
+
+object Game {
+    def main(args :Array[String]) {
+        board = Array("   ", "   ", "   ")
+        while(TicTac.whoWon(board) == "No winner yet!")
+    }
+}
