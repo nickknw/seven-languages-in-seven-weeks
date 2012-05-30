b770db0212d36dc7ea12b9ad58ceda4d75b3a2ee
Nick Knowlson <nick@nickknowlson.com>
2012-05-30 16:14:07 -0700

    majorly revise sudoku solver

--- week-3-prolog/day3.pl
+++ week-3-prolog/day3.pl
@@ -42,68 +42,56 @@
 % Do:
 %
 % 1. Modify the Sudoku solver to work on six-by-six puzzles (squares are 3x2)
-% and 9x9 puzzles)
+% and 9x9 puzzles).
+%
+% I chose to just do 9x9:
 
     :- use_module(library(clpfd)).
 
-    sudoku(Puzzle) :- 
-    Puzzle = [S11, S12, S13, S14, S15, S16, S17, S18, S19,
-              S21, S22, S23, S24, S25, S26, S27, S28, S29,
-              S31, S32, S33, S34, S35, S36, S37, S38, S39,
+    sudoku(Rows) :-
+        % split into rows
+        Rows = [A, B, C, D, E, F, G, H, I],
+        % and columns
+        transpose(Rows, Columns), 
 
-              S41, S42, S43, S44, S45, S46, S47, S48, S49,
-              S51, S52, S53, S54, S55, S56, S57, S58, S59,
-              S61, S62, S63, S64, S65, S66, S67, S68, S69,
+        % some bounds checking
+        append(Rows, FlattenedRows), FlattenedRows ins 1..9,
+        length(Rows, 9),
+        length(Columns, 9),
 
-              S71, S72, S73, S74, S75, S76, S77, S78, S79,
-              S81, S82, S83, S84, S85, S86, S87, S88, S89,
-              S91, S92, S93, S94, S95, S96, S97, S98, S99],
+        % all rows valid
+        maplist(all_distinct, Rows),
 
-    Puzzle ins 1..9,
+        % all columns valid
+        maplist(all_distinct, Columns),
 
-    Row1 = [S11, S12, S13, S14, S15, S16, S17, S18, S19],
-    Row2 = [S21, S22, S23, S24, S25, S26, S27, S28, S29],
-    Row3 = [S31, S32, S33, S34, S35, S36, S37, S38, S39],
-    Row4 = [S41, S42, S43, S44, S45, S46, S47, S48, S49],
-    Row5 = [S51, S52, S53, S54, S55, S56, S57, S58, S59],
-    Row6 = [S61, S62, S63, S64, S65, S66, S67, S68, S69],
-    Row7 = [S71, S72, S73, S74, S75, S76, S77, S78, S79],
-    Row8 = [S81, S82, S83, S84, S85, S86, S87, S88, S89],
-    Row9 = [S91, S92, S93, S94, S95, S96, S97, S98, S99],
+        % all blocks valid
+        valid_blocks(A, B, C),
+        valid_blocks(D, E, F),
+        valid_blocks(G, H, I),
 
-    Col1 = [S11, S21, S31, S41, S51, S61, S71, S81, S91],
-    Col2 = [S12, S22, S32, S42, S52, S62, S72, S82, S92],
-    Col3 = [S13, S23, S33, S43, S53, S63, S73, S83, S93],
-    Col4 = [S14, S24, S34, S44, S54, S64, S74, S84, S94],
-    Col5 = [S15, S25, S35, S45, S55, S65, S75, S85, S95],
-    Col6 = [S16, S26, S36, S46, S56, S66, S76, S86, S96],
-    Col7 = [S17, S27, S37, S47, S57, S67, S77, S87, S97],
-    Col8 = [S18, S28, S38, S48, S58, S68, S78, S88, S98],
-    Col9 = [S19, S29, S39, S49, S59, S69, S79, S89, S99],
+        prettier_print(FlattenedRows).
 
-    Square1 = [S11, S12, S13, S21, S22, S23, S31, S32, S33],
-    Square2 = [S14, S15, S16, S24, S25, S26, S34, S35, S36],
-    Square3 = [S17, S18, S19, S27, S28, S29, S37, S38, S39],
+    valid_blocks([], [], []).  
+    valid_blocks([A1, A2, A3 | As], [B1, B2, B3 | Bs], [C1, C2, C3 | Cs]) :-
+        all_distinct([A1, A2, A3, B1, B2, B3, C1, C2, C3]),
+        valid_blocks(As, Bs, Cs).
 
-    Square4 = [S41, S42, S43, S51, S52, S53, S61, S62, S63],
-    Square5 = [S44, S45, S46, S54, S55, S56, S64, S65, S66],
-    Square6 = [S47, S48, S49, S57, S58, S59, S67, S68, S69],
 
-    Square7 = [S71, S72, S73, S81, S82, S83, S91, S92, S93],
-    Square8 = [S74, S75, S76, S84, S85, S86, S94, S95, S96],
-    Square9 = [S77, S78, S79, S87, S88, S89, S97, S98, S99],
+    problem(1, [[_,_,_,_,_,_,_,_,_],                                   
+                [_,_,_,_,_,3,_,8,5],                                   
+                [_,_,1,_,2,_,_,_,_],                                   
+                [_,_,_,5,_,7,_,_,_],                                   
+                [_,_,4,_,_,_,1,_,_],                                   
+                [_,9,_,_,_,_,_,_,_],                                  
+                [5,_,_,_,_,_,_,7,3],                                  
+                [_,_,2,_,1,_,_,_,_],                                   
+                [_,_,_,_,4,_,_,_,9]]).
 
-    valid([Row1, Row2, Row3, Row4, Row5, Row6, Row7, Row8, Row9,
-           Col1, Col2, Col3, Col4, Col5, Col6, Col7, Col8, Col9,
-           Square1, Square2, Square3, Square4, Square5, Square6, Square7, Square8, Square9]),
+    % can test with:
+    % ?- problem(1, Board), sudoku(Board).
 
-    label(Puzzle),
-    prettier_print(Puzzle).
-
-    valid([]).
-    valid([Head|Tail]) :-
-        all_different(Head),
-        valid(Tail).
+    %label(Rows),
 
 % 2. Make the Sudoku solver print prettier solutions.
 
@@ -141,37 +129,3 @@
 % by its position in the list and the column by the value in the list.
 
 % ...I think I'm going to leave this one.
-
-    valid_queen((_, Col)) :- member(Col, [1,2,3,4,5,6,7,8]).
-
-    valid_board([]).
-    valid_board([Head|Tail]) :- valid_queen(Head), valid_board(Tail).
-
-    cols([], []).
-    cols([(_, Col)|QueensTail], [Col|ColsTail]) :-
-        cols(QueensTail, ColsTail).
-
-    diags1([], []).
-    diags1([(Row, Col)|QueensTail], [Diagonal|DiagonalsTail]) :-
-        Diagonal is Col - Row,
-        diags1(QueensTail, DiagonalsTail).
-
-    diags2([], []).
-    diags2([(Row, Col)|QueensTail], [Diagonal|DiagonalsTail]) :-
-        Diagonal is Col + Row,
-        diags2(QueensTail, DiagonalsTail).
-
-    eight_queens(Board) :-
-        Board = [(1, _), (2, _), (3, _), (4, _), (5, _), (6, _), (7, _), (8, _)],
-        valid_board(Board),
-
-        cols(Board, Cols),
-        diags1(Board, Diags1),
-        diags2(Board, Diags2),
-
-        all_different(Cols),
-        all_different(Diags1),
-        all_different(Diags2).
-
-
-% Call with eight_queens(Board).
